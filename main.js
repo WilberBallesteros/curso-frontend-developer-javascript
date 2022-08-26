ebfd292c7fef3491aaeed4d3b24badeafdfc4817
Wilber Ballesteros <ballestas01@gmail.com>
2022-08-26 09:00:13 -0500

    se agrega funcionalidd de menu hamburguesa en mobile

--- main.js
+++ main.js
@@ -3,9 +3,19 @@
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+//
+const menuHmIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
+menuHmIcon.addEventListener('click', toggleMobileMenu);
+
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+//menu hamburguesa al dar clic muestre el menu
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
